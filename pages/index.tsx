cfd45f7fb50e53e72c6a566d630978ba61d9f13e
Jawad Ali <jawad.ali@repairdesk.co>
2023-01-29 11:58:55 +0500

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = ({ posts }: any) => {
+  return (
+    <div>
+      <Head>
+        <title>Multi step form</title>
+      </Head>
+      
+    </div>
+  );
+};
+
+export default Home;
